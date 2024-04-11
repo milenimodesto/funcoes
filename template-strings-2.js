b93bf57b3789c5c9f44d820515b0fd46bf8a7ef3
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-04-11 11:47:37 -0300

    Add files via upload

---
+++ template-strings-2.js
@@ -0,0 +1,7 @@
+const nome = "Mileni";
+const idade = 2024-2006;
+const cidade = "Peabiru"
+const apresentacao = "Meu nome é: " + nome + ", minha idade é:"
++ idade + ", e nasci na cidade de "
++ cidade + ".";
+console.log(apresentacao)
---
+++ template-strings-3.js
@@ -0,0 +1,5 @@
+const nome = "Mileni";
+const idade = 2024-2006;
+const cidade = "Peabiru"
+const apresentacao = `Meu nome é ${nome}, minha idade é: ${idade} e nasci em ${cidade}.`
+console.log(apresentacao)
